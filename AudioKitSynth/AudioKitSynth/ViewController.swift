d87e98ddadce503ca1ce9eb2b0ab600948a12a66
Austin Fletcher <afletcher@firstorion.com>
2019-03-09 20:37:46 -0600

    added frequency slider and button start and stop toggling

--- AudioKitSynth/AudioKitSynth/ViewController.swift
+++ AudioKitSynth/AudioKitSynth/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let oscillator = AKOscillator()
+    
+    // MARK: - Actions
+    
+    @IBAction func frequencySliderChanged(_ sender: UISlider) {
+        oscillator.frequency = sender.value * 880.0
+    }
+    
+    
+    @IBAction func playTouchStarted(_ sender: Any) {
+        oscillator.start()
+    }
+    
+    @IBAction func playTouchEnded(_ sender: Any) {
+        oscillator.stop()
+    }
+    
+    
+    
+    // MARK: - View lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        oscillator.amplitude = 0.3
+        AudioKit.output = oscillator
+        
+        try? AudioKit.start()
     }
 
 
