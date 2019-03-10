35f5c17c654bd5be6e8a3e01ca5c52072746611b
Austin Fletcher <afletcher@firstorion.com>
2019-03-09 20:11:48 -0600

    Initial hello world with audiokit

---
+++ AudioKitSynth/AudioKitSynth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioKitSynth
+//
+//  Created by Austin Fletcher on 3/9/19.
+//  Copyright © 2019 Austin Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
